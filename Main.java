a75393522a5584c0ce8338e715b6e4ec9029e761
AlumnoDAM <adriancalventemartin2012@gmail.com>
2021-09-30 13:57:29 +0200

    primera implementacion

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
-
+import dao.PokemonDAOImp;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		
-		
+		PokemonDAOImp pk=new PokemonDAOImp(5);
+		pk.escribirPokemon("./test.txt","bulva",5,20,15,2,3,120);
+
+
 
 	}
 
