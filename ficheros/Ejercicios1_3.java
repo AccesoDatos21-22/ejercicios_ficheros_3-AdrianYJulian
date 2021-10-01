4afe0db4a640a617ad41235cbe82b099417ff69d
Julian <juliandejuan22@gmail.com>
2021-10-01 19:30:44 +0200

    Ejercicio3 1.0

--- ficheros/Ejercicios1_3.java
+++ ficheros/Ejercicios1_3.java
@@ -1,65 +1,73 @@
 package ficheros;
 
+import interfaces.InterfazEjercicios1_3;
+
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import interfaces.InterfazEjercicios1_3;
-
 /**
- * 
  * @author Escribe_aqui_tu_nombre
- * @date 
- * @version 
+ * @date
  * @license GPLv3
- *
  */
 public class Ejercicios1_3 implements InterfazEjercicios1_3 {
 
-	
-	
-	@Override
-	public List<String> getFrases(Scanner escaner) {
-		
-		return null;
+
+    @Override
+    public List<String> getFrases(Scanner escaner) {
+        String continuar = "";
+        List<String> lista = new ArrayList<>();
+        System.out.println("Si desea salir introduzca -1");
+        while (!continuar.equals("-1")) {
+			System.out.println("Introduzca una frase");
+			continuar = escaner.nextLine();
+            if (!continuar.equals("-1")) {
+                lista.add(escaner.nextLine());
+            }
+        }
+        return lista;
+    }
+
+    @Override
+    public Path getNombre(Scanner escaner) {
+        System.out.println("introduzca el nombre del fichero deseado");
+        return Paths.get(escaner.nextLine());
+    }
+
+    @Override
+    public void escribefrases(List<String> cadenas, Path ruta) {
+		try {
+			cadenas=Files.readAllLines(ruta);
+			cadenas.forEach(System.out::println);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
-	@Override
-	public Path getNombre(Scanner escaner) {
-		
-		return null;
-	}
-	
-	@Override
-	public void escribefrases(List<String> cadenas, Path ruta) {
-		
-		
-	}
+    @Override
+    public void leerFrases(Path ruta) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void leerFrases(Path ruta) {
-		// TODO Auto-generated method stub
-		
-	}
+    }
 
 
+    @Override
+    public void escribirFlotante(float numeroDecimal, String ruta) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void escribirFlotante(float numeroDecimal, String ruta) {
-		// TODO Auto-generated method stub
-		
-	}
+    }
 
-	@Override
-	public List<Float> leerFlotante(String ruta) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	
-	
-		
-		
+    @Override
+    public List<Float> leerFlotante(String ruta) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 
 }
