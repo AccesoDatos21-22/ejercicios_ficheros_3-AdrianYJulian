c76090c007d7822ca98e80ab81c6a67055f109b1
Julian <juliandejuan22@gmail.com>
2021-10-03 19:13:17 +0200

    Ejercicio9

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
+import ficheros.Ejercicios1_3;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Ejercicios1_3 ejercicios1_3 = new Ejercicios1_3();
+        ejercicios1_3.escribirFlotante((float) 1.3123132131232131, "test.txt");
     }
 }
--- ficheros/Ejercicios1_3.java
+++ ficheros/Ejercicios1_3.java
@@ -1,65 +1,72 @@
 package ficheros;
 
+import interfaces.InterfazEjercicios1_3;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
 import java.util.Scanner;
 
-import interfaces.InterfazEjercicios1_3;
-
 /**
- * 
  * @author Escribe_aqui_tu_nombre
- * @date 
- * @version 
+ * @date
  * @license GPLv3
- *
  */
 public class Ejercicios1_3 implements InterfazEjercicios1_3 {
 
-	
-	
-	@Override
-	public List<String> getFrases(Scanner escaner) {
-		
-		return null;
-	}
 
-	@Override
-	public Path getNombre(Scanner escaner) {
-		
-		return null;
-	}
-	
-	@Override
-	public void escribefrases(List<String> cadenas, Path ruta) {
-		
-		
-	}
+    @Override
+    public List<String> getFrases(Scanner escaner) {
 
-	@Override
-	public void leerFrases(Path ruta) {
-		// TODO Auto-generated method stub
-		
-	}
+        return null;
+    }
+
+    @Override
+    public Path getNombre(Scanner escaner) {
+
+        return null;
+    }
+
+    @Override
+    public void escribefrases(List<String> cadenas, Path ruta) {
 
 
+    }
 
-	@Override
-	public void escribirFlotante(float numeroDecimal, String ruta) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void leerFrases(Path ruta) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public List<Float> leerFlotante(String ruta) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    }
 
-	
-	
-		
-		
+
+    @Override
+    public void escribirFlotante(float numeroDecimal, String ruta) {
+        Path archivoFlotantes = Paths.get(ruta);
+        if (!Files.exists(archivoFlotantes)) {
+            try {
+                Files.createFile(archivoFlotantes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try (BufferedWriter bufferedWriter = Files.newBufferedWriter(archivoFlotantes, StandardOpenOption.APPEND)) {
+                bufferedWriter.write(String.valueOf(numeroDecimal));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public List<Float> leerFlotante(String ruta) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 
 }
