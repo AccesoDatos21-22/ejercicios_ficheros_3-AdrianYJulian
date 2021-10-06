69a14b2ef06aac54b735a472e77b14e93f229873
AlumnoDAM <adriancalventemartin2012@gmail.com>
2021-10-06 10:12:54 +0200

    Ejercicio 14 finalizado

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
-import ficheros.Ejercicios1_3;
 import modelo.Pokemon;
 
 public class Main {
 
     public static void main(String[] args) {
         Pokemon p1 = new Pokemon(2);
-
         p1.escribirPokemon("test.txt",p1);
+        for (int i = 1; i <30 ; i++) {
+            p1.escribirPokemon("test.txt",new Pokemon(i));
+        }
+
+        p1.leerPokemon("test.txt").forEach(System.out::println);
     }
 }
