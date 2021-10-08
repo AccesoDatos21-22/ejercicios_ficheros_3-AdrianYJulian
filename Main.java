a878e47566a943fcd7748e949f566fad328dfeb2
Julian <juliandejuan22@gmail.com>
2021-10-08 18:18:44 +0200

    Ejercicio11 test

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ public class Main {
         Ejercicios1_3 ejercicios1_3 = new Ejercicios1_3();
         ejercicios1_3.escribirFlotante((float) 1.3123132131232131, "test.dat");
         ejercicios1_3.imprimirFlotante("test.dat");
+        ejercicios1_3.leerFlotante("test.dat");
     }
 }
